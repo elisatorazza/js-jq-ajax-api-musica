13285850837189b60d93a8a46196f7e7a2035abb
Elisa <elisa.torazza@gmail.com>
2020-09-18 11:06:48 +0200

    Aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -5,25 +5,38 @@ $(document).ready(function() {
     "url": "https://flynn.boolean.careers/exercises/api/array/music",
     "method": "GET",
     "success": function (data, stato) {
-      console.log(data["response"]);
       var oggettoAlbum = data["response"];
-
-      // var source =$(".entry-template").html();
-      // var template = Handlebars.compile(source);
-
-      for (var i=0; i<oggettoAlbum.length; i++){
-        console.log(oggettoAlbum[i]);
-        var context = oggettoAlbum[i];
-        // var html = template(context);
-      }
-
+      render(oggettoAlbum);
     },
     "error": function (richiesta, stato, errori) {
       alert();
     }
+    }
+  );
+
+  function render(oggettoAlbum) {
+    var source =$("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    for (var i=0; i<oggettoAlbum.length; i++){
+      var context = oggettoAlbum[i];
+      var html = template(context);
+      $(".cds-container").append(html);
+    }
+  };
+
+   $(".genre").change(function(){
+     var selectGenre = $(this).val();
+     console.log(selectGenre);
+     if (selectGenre == "All") {
+      $(".cd").show();
+     } else {
+      $(".cd").addClass("none");
+      $(".cd[data-genere = '"+selectGenre+"']").removeClass("none");
+    }
+  });
+
+  function selectGenre(value, key) {
+
   }
-  )
-
-
-
 });
